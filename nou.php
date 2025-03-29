02fa1fcf2d450abd68c88c63ec5a39cfe3625900
Macia <macia_llobera@hotmail.es>
2025-03-29 19:06:24 +0100

    Modificada versió de nou.php a develop per generar conflicte

--- nou.php
+++ nou.php
@@ -1,3 +1,5 @@
 <?php
-echo "Hola, aquest és el nou script!";
+echo "Això és la versió modificada de nou.php a develop";
+// Afegim una línia extra per provocar conflicte:
+echo "Línia afegida a develop per provocar conflicte";
 ?>
